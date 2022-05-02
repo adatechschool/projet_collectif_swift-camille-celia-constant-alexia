b06c38e170c4e61883630d7545d496f47e25da29
Camille <folcamille@yahoo.fr>
2022-05-02 12:05:53 +0200

    Initial Commit

---
+++ CCCA/CCCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCCAApp.swift
+//  CCCA
+//
+//  Created by Camille FOL on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CCCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCCA
+//
+//  Created by Camille FOL on 02/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
